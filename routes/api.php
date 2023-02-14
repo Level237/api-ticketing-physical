abe956394ed06f375f5a316f511be94148f0421a
Level237 <Bramslevel129@gmail.com>
2023-02-14 17:40:02 +0100

    update

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('add/passenger/{id}',[PassengerController::class,'store']);
+Route::post('add/passenger/{id}/{sub_agency_id}',[PassengerController::class,'store']);
